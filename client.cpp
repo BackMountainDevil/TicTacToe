48f9b9bbc913a45f382cb389544974869bb736e9
kearney <191615342@qq.com>
2021-07-13 13:20:11 +0800

    无法连接服务器时启动人机对战

--- client.cpp
+++ client.cpp
@@ -1,11 +1,10 @@
+#ifdef __linux__
+const int OS = 0;
+#elif defined(_WIN32)
+const int OS = 1;
+#endif
 #include "client.h"
 
-/* 多线程 I/O 分离 群聊客户端 */
-char bufSend[BUF_SIZE] = {0};
-char bufRecv[BUF_SIZE] = {0};
-int vssocket;
-
-// 创建套接字
 Client::Client() {
   // 由域名获取 IP
   struct hostent *host = gethostbyname(DOMAIN);
@@ -33,7 +32,6 @@ Client::Client() {
   }
 }
 
-// 连接服务器
 bool Client::Connect() {
   if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
     perror("Error: Connection creation failed");
@@ -44,7 +42,6 @@ bool Client::Connect() {
   }
 }
 
-// 开始游戏
 bool Client::Start() {
   if (this->Connect()) {
     if (read(sock, bufRecv, sizeof(bufRecv)) == -1) {
@@ -77,13 +74,149 @@ bool Client::Start() {
       }
     }
   } else {
-    // AI
+    this->PlayAI();
   }
-
   return false;
 }
 
-// 关闭套接字
+void Client::showBoard() {
+  // 显示棋盘以及棋子
+  for (int i = 0; i < 9; i++) {
+    std::cout << " __";
+    if (BOARD[i] == 0) {
+      std::cout << i;
+    } else {
+      if (BOARD[i] == 1) {
+        std::cout << 'x';
+      } else {
+        std::cout << 'A';
+      }
+    }
+
+    if ((i + 1) % 3 == 0) {
+      std::cout << "__" << std::endl;
+    } else {
+      std::cout << "__ |";
+    }
+  }
+}
+
+int Client::checkInput() {
+  int pos = -1;
+  while (pos < 0 || pos > 8) {
+    std::cin >> pos;
+    if (BOARD[pos] != 0) {
+      std::cout << "Position is not null, try again" << std::endl;
+      pos = -1;
+    }
+  }
+  return pos;
+}
+
+int Client::checkWinner() {
+  // 检测赢家，返回值 -1（无赢家） or 1（玩家一） or 2（玩家二）
+  int winner = -1;
+  for (int i = 0; i < 3; i++) {
+    if (BOARD[i] == BOARD[i + 3] && BOARD[i + 3] == BOARD[i + 6] &&
+        BOARD[i] != 0) { // 竖
+      winner = BOARD[i];
+      break;
+    } else if (BOARD[i * 3] == BOARD[i * 3 + 1] &&
+               BOARD[i * 3 + 1] == BOARD[i * 3 + 2] &&
+               BOARD[i * 3] != 0) { // 横
+      winner = BOARD[i * 3];
+      break;
+    }
+  }
+  // 对角线
+  if (BOARD[0] == BOARD[4] && BOARD[4] == BOARD[8] && BOARD[4] != 0) {
+    winner = BOARD[4];
+  } else if (BOARD[2] == BOARD[4] && BOARD[4] == BOARD[6] && BOARD[4] != 0) {
+    winner = BOARD[4];
+  }
+  return winner;
+}
+
+void Client::clearScreen() {
+  if (OS == 0)
+    system("clear");
+  else
+    system("clr");
+}
+
+int Client::AIInput() {
+  unsigned int i = 0;
+  bool found = false;
+  while (!found && i < 9) {
+    if (BOARD[i] == 0) {
+      BOARD[i] = 2; // 一下能赢的棋
+      if (checkWinner() == 2) {
+        found = true;
+      }
+      BOARD[i] = 0;
+
+      BOARD[i] = 1; // 不下会输的棋
+      if (checkWinner() == 1) {
+        found = true;
+      }
+      BOARD[i] = 0;
+    }
+    i++;
+  }
+
+  if (!found) { //第三种策略
+    const int next[9] = {4, 0, 2, 6, 8, 1, 3, 5, 7};
+    unsigned int k = 0;
+    while (!found && k < 9) {
+      if (BOARD[next[k]] == 0) {
+        found = true;
+      }
+      k++;
+    }
+    return k - 1;
+  } else {
+    return i - 1; // 前两中策略有效
+  }
+}
+
+void Client::PlayAI() {
+  showBoard();
+  int winner = -1;
+
+  int count = 0, turn;
+  while (count < 9) {
+    int pos;
+    turn = count % 2 + 1;
+    if (turn == 2) {
+      pos = AIInput();
+    } else {
+      std::cout << "Player " << turn << " : ";
+      pos = checkInput();
+    }
+    BOARD[pos] = turn;
+    clearScreen();
+    showBoard();
+    winner = checkWinner();
+    if (winner != -1) {
+      break;
+    }
+    count++;
+  }
+
+  switch (winner) {
+  case 1:
+    std::cout << "YOU WIN!" << std::endl;
+    break;
+  case 2:
+    std::cout << "you lose!" << std::endl;
+    break;
+  case -1:
+    std::cout << "Nobody win" << std::endl;
+    break;
+  }
+  std::cout << "Game Over. Take a rest" << std::endl;
+}
+
 void Client::Close() { close(this->sock); }
 
 int main() {
--- client.h
+++ client.h
@@ -4,12 +4,20 @@
 
 class Client {
 public:
-  Client();
-  bool Connect();
-  bool Start();
-  void Close();
+  Client();         // 创建套接字
+  void showBoard(); // 显示棋盘以及棋子
+  int checkInput(); // 获取用户的合法输入
+  int checkWinner(); // 检测赢家，返回值 -1（无赢家） or 1（玩家一） or
+  // 2（玩家二）
+  void clearScreen(); // 清空控制台所有内容, linux 与 windows 有所不同
+  int AIInput();      // AI 输入
+  bool Connect();     // 连接服务器
+  bool Start();       // 开始游戏
+  void PlayAI();      // 人机对战
+  void Close();       // 关闭套接字
 
 private:
+  int BOARD[9] = {0, 0, 0, 0, 0, 0, 0, 0, 0};
   int sock;
   char bufSend[BUF_SIZE];
   char bufRecv[BUF_SIZE];
